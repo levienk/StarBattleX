e88f09a4848c393e72ca8edfdcf7d6a7336963b3
Daniel Ma <91981771+PLUMaD@users.noreply.github.com>
2023-10-23 12:11:48 -0700

    Implementing fields and methods for Square
    Implemented the state field, isStarValid field, and setState method.

--- src/main/java/starb/client/domain/Square.java
+++ src/main/java/starb/client/domain/Square.java
@@ -1,4 +1,10 @@
 package starb.client.domain;
 
 public class Square {
+    private String state = "";
+    private boolean isStarValid = true;
+
+    protected void setState(String state){
+        this.state = state;
+    }
 }
