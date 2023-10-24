b89cd59762fddb9e02fe1786f75a0263ec3b3bf6
Kade Levien <kade.levien@gmail.com>
2023-10-23 22:59:37 -0700

    Create temporary board in JSONReader

--- src/main/java/starb/client/domain/json/JSONReader.java
+++ src/main/java/starb/client/domain/json/JSONReader.java
@@ -1,18 +1,16 @@
 package starb.client.domain.json;
 
 import javafx.geometry.Point2D;
+import starb.client.domain.game.Board;
 import starb.client.domain.game.Square;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.EventListener;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class JSONReader {
     // The puzzle board loaded from the JSON
-//    private Board board;
+    private Board board;
     // The JSON file with the puzzle data
     private File file;
     // Scanner to read from the file
@@ -23,6 +21,15 @@ public class JSONReader {
     private String puzzleName;
 
     public JSONReader(String fileName) throws FileNotFoundException {
+        // Temporary initialization of the Board
+        // Please do not remove until implemented
+        List<HashMap<Point2D, Square>> sections = new ArrayList<>();
+        HashMap<Point2D, Square> section = new HashMap<>();
+        section.put(new Point2D(2,3), new Square());
+        sections.add(section);
+        // The Board expects sections to be filled, so this will cause problems
+        board = new Board(10, 10, sections, "Temporary Board");
+
         // JSON file with a puzzle
         this.file = new File(fileName);
         // Reads from file
@@ -41,8 +48,8 @@ public class JSONReader {
         return null;
     }
 
-//    private Board getBoard(){
-//        // return the Board object
-//        return this.board;
-//    }
+    private Board getBoard(){
+        // return the Board object
+        return this.board;
+    }
 }
