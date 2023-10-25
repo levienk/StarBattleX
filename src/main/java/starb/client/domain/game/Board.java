3636b36be9f864847cae513cc1347fc73dfc47aa
Max Lopez <familiacachorin@gmail.com>
2023-10-25 11:44:16 -0700

    Updated Board class and relevant tests in starb.client.domain.game package
    - Renamed `puzzleName` to `PUZZLENAME` and made it final.
    - Introduced a new list `validStars` to track valid star positions.
    - Enhanced the `updateSquare` method to handle multiple states and update `invalidStars` and `validStars` lists accordingly.
    - Adjusted the `checkRow`, `checkColumn`, and `checkSection` methods to handle a star count of exactly 2, rather than more than 2.
    - Modified `checkSquare` method to update the `validStars` list based on validation checks.
    - Adjusted the `isComplete` method to check for 20 valid stars with the correct state.
    - Added getter methods for `sections`, `validStars`, `squares`, and `invalidStars`.
    - Updated BoardTest to reflect the changes and added new tests for the introduced functionalities.
    - Made necessary adjustments in the Gradle configurations to handle the updated classes and dependencies.

--- src/main/java/starb/client/domain/game/Board.java
+++ src/main/java/starb/client/domain/game/Board.java
@@ -17,7 +17,8 @@ public class Board {
     private final int COLUMNS;
 
     private List<Point2D> invalidStars;
-    private String puzzleName;
+    private List<Point2D> validStars;
+    private final String PUZZLENAME;
     public Board(int rows, int columns, List<HashMap<Point2D, Square>> sections, String puzzleName) {
         ROWS = rows;
         COLUMNS = columns;
@@ -30,18 +31,55 @@ public class Board {
         }
 
         //set name
-        this.puzzleName = puzzleName;
+        this.PUZZLENAME = puzzleName;
 
         // Initialize invalidStars list
         this.invalidStars = new ArrayList<>();
+
+        // Initialize validStars list
+        this.validStars = new ArrayList<>();
 
 
     }
     public void updateSquare(Point2D point, String state) {
         if(state.equals("star") && checkSquare(point)) {
             squares.get(point).setState("star");
+            if(invalidStars.contains(point)) {
+                invalidStars.remove(point);
+            }
+            validStars.add(point);
+
         } else if ( state.equals("dot") ) {
             squares.get(point).setState("dot");
+            //remove from both lists
+            if(validStars.contains(point)) {
+                validStars.remove(point);
+            }
+            if(invalidStars.contains(point)) {
+                invalidStars.remove(point);
+            }
+        } else if (state.equals("")) {
+            validStars.remove(point);
+            squares.get(point).setState("");
+            //remove from both lists
+            if(validStars.contains(point)) {
+                validStars.remove(point);
+            }
+            if(invalidStars.contains(point)) {
+                invalidStars.remove(point);
+            }
+        }
+    }
+    private boolean checkSquare(Point2D point) {
+        if (checkArea(point) && checkSection(point) && checkColumn((int) point.getX()) && checkRow((int) point.getY())) {
+            return true;
+        }
+        else {
+            if(validStars.contains(point)) {
+                validStars.remove(point);
+            }
+            invalidStars.add(point);
+            return false;
         }
     }
     private boolean checkRow(int row) {
@@ -50,7 +88,7 @@ public class Board {
             if(squares.get(new Point2D(i, row)).getState().equals("star")) {
                 starCount++;
             }
-            if (starCount > 2) {
+            if (starCount == 2) {
                 return false;
             }
         }
@@ -62,7 +100,7 @@ public class Board {
             if(squares.get(new Point2D(column, i)).getState().equals("star")) {
                 starCount++;
             }
-            if(starCount > 2) {
+            if(starCount == 2) {
                 return false;
             }
         }
@@ -74,7 +112,7 @@ public class Board {
             if (entry.getValue().getState().equals("star")) {
                 starCount++;
             }
-            if(starCount > 2) {
+            if(starCount == 2) {
                 return false;
             }
         }
@@ -88,6 +126,7 @@ public class Board {
         }
         return null;
     }
+    //working
     private boolean checkArea(Point2D point) {
         for(int i = -1; i <= 1; i++) {
             for(int j = -1; j <= 1; j++) {
@@ -105,29 +144,18 @@ public class Board {
         }
         return true;
     }
-    private boolean checkSquare(Point2D point) {
-        if (checkArea(point) && checkSection(point) && checkColumn((int) point.getX()) && checkRow((int) point.getY())) {
-            return true;
-        } else {
-            invalidStars.add(point);
+
+    public boolean isComplete() {
+        if(validStars.size() != 20) {
             return false;
         }
-
-    }
-    public boolean isComplete() {
-        int starCount = 0;
-        for (Map.Entry<Point2D, Square> entry: squares.entrySet()) {
-            if (entry.getValue().getState().equals("star") && entry.getValue().isStarValid()) {
-                starCount++;
+        for (Point2D starPoint : validStars) {
+            Square square = squares.get(starPoint);
+            if (!square.getState().equals("star")) {
+                return false;
             }
         }
-        if(starCount == (ROWS * 2)) {
-            return true;
-        }
-        return false;
-    }
-    public List<Point2D> getInvalidStars() {
-        return invalidStars;
+        return true;
     }
     public List<Line> getSectionBoundaries() {
         ArrayList<Line> allSectionLines = new ArrayList<>();
@@ -172,7 +200,7 @@ public class Board {
     }
 
     public String getPuzzleName() {
-        return puzzleName;
+        return PUZZLENAME;
     }
     public int getRows() {
         return ROWS;
@@ -180,4 +208,11 @@ public class Board {
     public int getColumns() {
         return COLUMNS;
     }
+    public List<HashMap<Point2D, Square>> getSections() { return sections;}
+    public List<Point2D> getValidStars() { return validStars;}
+    public HashMap<Point2D, Square> getSquares() { return squares;}
+    public List<Point2D> getInvalidStars() {
+        return invalidStars;
+    }
+
 }
