a88fd4f69685656b1e90814dce8faaf625d11eb6
Randy Nguyen <pronexusgg@gmail.com>
2023-10-24 18:28:43 -0700

    More thorough fix for switching to different scenes with varying parameters.

--- src/main/java/starb/client/SceneSwitcher.java
+++ src/main/java/starb/client/SceneSwitcher.java
@@ -21,6 +21,19 @@ public class SceneSwitcher {
     private SceneSwitcher() {
     }
 
+    /**
+     *  Sets the scene to the given scene.
+     *  If the scene has not been created yet, then it will create it.
+     *  If the scene has been created, then it will just set the scene to that.
+     *
+     *
+     * @param scene The scene to set to.
+     * @param params The parameters to pass to the constructor of the scene.
+     * @throws NoSuchMethodException If there is no constructor that matches the given parameters.
+     * @throws InvocationTargetException If the constructor throws an exception.
+     * @throws InstantiationException If the constructor throws an exception.
+     * @throws IllegalAccessException If the constructor throws an exception.
+     */
     public static void setScene(Class<? extends Parent> scene, Object... params) throws
             NoSuchMethodException, InvocationTargetException, InstantiationException,
             IllegalAccessException {
@@ -29,8 +42,58 @@ public class SceneSwitcher {
         }
 
         if (!scenes.containsKey(scene)) {
+
+            boolean foundMatchingConstructor = false;
+
+            Constructor<?> cons = null;
+
+            /*
+             *  When creating a new scene, we need to find the constructor in that class
+             *  that matches the parameters we are given.
+             *
+             *  For some scenes, there are constructors with varying parameters.
+             *  For example, the PuzzleScene class has a constructor that takes in a
+             *  Board object and a String object.
+             *
+             *  This also checks to see if we can actually create a scene with the given
+             *  parameters.
+             *
+             *  If we can't, then we throw a NoSuchMethodException.
+             */
+
+            for (int i = 0; i < scene.getDeclaredConstructors().length; i++) {
+
+
+                // oh, god this is ugly
+                cons = scene.getDeclaredConstructors()[i];
+
+                // These question marks are a mystery to me.
+                Class<?>[] conClasses = new Class<?>[cons.getParameterCount()];
+
+                for (int j = 0; j < cons.getParameterCount(); j++) {
+                    conClasses[j] = cons.getParameters()[j].getType();
+                }
+
+                Class<?>[] paramsClasses = new Class<?>[params.length];
+
+                for (int j = 0; j < params.length; j++) {
+                    paramsClasses[j] = params[j].getClass();
+                }
+
+                if (Arrays.equals(conClasses, paramsClasses)) {
+                    foundMatchingConstructor = true;
+                    break;
+                }
+            }
+
+            if (!foundMatchingConstructor) {
+
+                throw new NoSuchMethodException("No constructor found for " + scene.getName() +
+                        " with the given parameters.");
+            }
+
             Scene initScene =
-                    new Scene((Parent) scene.getDeclaredConstructors()[0].newInstance(params));
+                    new Scene((Parent) cons.newInstance(params));
             scenes.put(scene, initScene);
         }
 
