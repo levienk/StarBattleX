877f96e6d2f8287449e9ea2d3b7d2d621f665a0f
Max Lopez <familiacachorin@gmail.com>
2023-10-25 23:16:10 -0700

    update Board to update affected stars when star is placed

--- src/main/java/starb/client/domain/game/Square.java
+++ src/main/java/starb/client/domain/game/Square.java
@@ -6,11 +6,18 @@ public class Square {
 
     public Square(){
         this.state = "";
-        this.isStarValid = true;
+        this.isStarValid = false;
     }
 
     protected void setState(String state){
         this.state = state;
     }
     public String getState() { return state;}
+    public void setStarValidity(boolean validity) {
+        this.isStarValid = validity;
+    }
+
+    public boolean getStarValidity() {
+        return isStarValid;
+    };
 }
