80e490c28ea1f4a229b7146f2c112d41904dd310
Kade Levien <kade.levien@gmail.com>
2023-11-04 13:37:01 -0700

    Fix temporary Board to update the parameter

--- src/main/java/starb/domain/json/JSONReader.java
+++ src/main/java/starb/domain/json/JSONReader.java
@@ -22,7 +22,8 @@ public class JSONReader {
     public JSONReader(String fileName) throws FileNotFoundException {
         // Temporary initialization of the Board
         // Please do not remove until implemented
-        board = new Board(10, 10, getTemporarySections(), 1);
+        HashSet<Point> solution = new HashSet<>();
+        board = new Board(10, 10, getTemporarySections(), solution, 1);
 
         // JSON file with a puzzle
         //this.file = new File(fileName);
