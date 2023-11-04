7560dc3d911fb4355d458e75dd61a47cd02eba38
Daniel Ma <91981771+PLUMaD@users.noreply.github.com>
2023-11-04 13:39:56 -0700

    Created Board Repository
    Created base file for Board Repository

---
+++ src/main/java/starb/server/repo/boardsRepository.java
@@ -0,0 +1,11 @@
+package starb.server.repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import starb.domain.game.Board;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface boardsRepository extends MongoRepository<Board, String>{
+    
+}
