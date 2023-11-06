fd337d807d698d46fa7eed061ff5e50a332ca343
Daniel Ma <91981771+PLUMaD@users.noreply.github.com>
2023-11-05 17:20:14 -0800

    Updated User.Java
    Updated the User constructor for server to client communication.

--- src/main/java/starb/server/controller/ExampleController.java
+++ src/main/java/starb/server/controller/ExampleController.java
@@ -1,8 +1,10 @@
 package starb.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import starb.server.repo.UserRepository;
 
 @RestController
 @RequestMapping(path="example")
--- src/main/java/starb/server/controller/UserController.java
+++ src/main/java/starb/server/controller/UserController.java
@@ -1,13 +1,39 @@
 package starb.server.controller;
 
+import javafx.application.Application;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import starb.domain.json.User;
+import starb.server.repo.UserRepository;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
-    /**
-    private TodoRepository repo;
-    public TodoController(TodoRepository repo) {
-        this.repo = repo;
-    }*/
+    @Autowired
+    private UserRepository users;
+
+    @PostMapping
+    public User postUser(){
+        User doc = new User();
+        users.save(doc);
+        return null;
+    }
+
+    @RequestMapping("{id}")
+    @PutMapping
+    public User putUser(@PathVariable String id, @RequestBody User user){
+        return null;
+    }
+
+    @RequestMapping("{id}")
+    @GetMapping(produces="application/json")
+    public User getUser(@PathVariable String id) {
+        return null;
+    }
+
+    @RequestMapping("{id}")
+    @DeleteMapping
+    public void deleteUser(@PathVariable String id){
+        //
+    }
 }
