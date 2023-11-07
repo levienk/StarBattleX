9f58d2619723b303f56f17710e219781b2dcec0b
Daniel Ma <91981771+PLUMaD@users.noreply.github.com>
2023-11-06 17:22:41 -0800

    Implemented UserController
    Implemented PUT, GET, and DELETE methods

--- src/main/java/starb/server/repo/UserRepository.java
+++ src/main/java/starb/server/repo/UserRepository.java
@@ -1,7 +1,10 @@
 package starb.server.repo;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import starb.domain.json.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    @Query("{'_id' : ?0}")
+    User updateField(String id, String newValue);
 }
