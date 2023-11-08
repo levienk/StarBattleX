867800baddaf65b836e2d1a80ef0d42fa9b82585
Daniel Ma <91981771+PLUMaD@users.noreply.github.com>
2023-11-07 17:56:43 -0800

    Added Package for authorization header
    Added client filter and filter bean config.

---
+++ src/main/java/starb/server/filter/FilterBeanConfig.java
@@ -0,0 +1,23 @@
+package starb.server.filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterBeanConfig {
+
+        @Bean
+        public FilterRegistrationBean<ClientIdFilter> usernameFilterBean() {
+            ClientIdFilter userFilter=new ClientIdFilter();
+            final FilterRegistrationBean<ClientIdFilter> reg = new FilterRegistrationBean<>(userFilter);
+
+            // ************************************************
+            // Configure here which paths require authentication
+            reg.addUrlPatterns("/example/one");
+            // ************************************************
+
+            reg.setOrder(1);   //defines filter execution order
+            return reg;
+        }
+}
