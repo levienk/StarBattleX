5b83c0c5cd58cd50ba0a5e81ecc03b35fce2e6ea
Daniel Ma <91981771+PLUMaD@users.noreply.github.com>
2023-11-17 16:19:44 -0800

    Sprint 4
    Fixed Http Status Code response format and JUnit Tests
    
    Altered the files to work for updated Status Codes

--- src/main/java/starb/server/controller/UserController.java
+++ src/main/java/starb/server/controller/UserController.java
@@ -3,29 +3,43 @@ package starb.server.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import starb.domain.game.Board;
 import starb.domain.json.User;
+import starb.server.repo.BoardRepository;
 import starb.server.repo.UserRepository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
     private final UserRepository users;
+    private final BoardRepository boards;
 
-    public UserController(UserRepository users) {
+    public UserController(UserRepository users, BoardRepository boards) {
         this.users = users;
+        this.boards = boards;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User postUser(){
         User doc = new User();
+        // Grab the board ids
+        List<Integer> boardIds = boards.findAll()
+                .stream().map(Board::getID)
+                .toList();
+        doc.setInaccessible(boardIds); // Populate the inaccessible list
+        System.out.println(doc.getInaccessible());
         return users.save(doc);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<User> putUser(@PathVariable String id, @RequestBody User userInput){
+    @ResponseStatus(HttpStatus.OK)
+    public User putUser(@PathVariable String id, @RequestBody User userInput){
         try {
             // The user identified by the id
             User updatedUser;
@@ -34,26 +48,30 @@ public class UserController {
                 // Perform the update on a field in updatedUser
                 updatedUser = users.save(userInput);
                 // Return the updated User with a 200 OK response
-                return ResponseEntity.ok(updatedUser);
+                return updatedUser;
             }
             // Return a 404 Not Found response if the user with the specified ID was not found
-            return ResponseEntity.notFound().build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No User with matching ID");
         } catch (Exception e) {
             // Handle exceptions or validation errors
-            return ResponseEntity.badRequest().build(); // Return a 400 Bad Request response for errors
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST); // Return a 400 Bad Request response for errors
         }
     }
 
     @GetMapping(value = "{id}", produces = "application/json")
-    public ResponseEntity<User> getUser(@PathVariable String id) {
+    @ResponseStatus(HttpStatus.OK)
+    public User getUser(@PathVariable String id) {
         Optional<User> target = users.findById(id);
-
+        if (target.isEmpty()){
+            // Return 404 Not Found
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No User with matching ID");
+        }
         // Return 200 OK with the User object
-        // Return 404 Not Found
-        return target.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return target.get();
     }
 
     @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable String id){
         // Check if the id is in use
         if (users.existsById(id)) {
--- src/test/java/starb/server/UserControllerTests.java
+++ src/test/java/starb/server/UserControllerTests.java
@@ -1,24 +1,36 @@
 package starb.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import starb.domain.json.User;
 import starb.server.controller.UserController;
 import starb.server.repo.UserRepository;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class UserControllerTests {
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private TestRestTemplate restTemplate;
     private User existingUser;
 
     @Autowired
@@ -26,6 +38,7 @@ public class UserControllerTests {
 
     @Autowired
     private UserController controller;
+    private HttpClient client = HttpClient.newHttpClient();
 
     @BeforeEach
     void setUp() {
@@ -41,7 +54,7 @@ public class UserControllerTests {
 
         assertNotNull(newUser.getId());
 
-        assertEquals("", newUser.getNextPuzzle());
+        assertEquals(-1, newUser.getNextPuzzle());
 
         assertTrue(newUser.getCompleted().isEmpty());
         assertTrue(newUser.getInaccessible().isEmpty());
@@ -49,44 +62,74 @@ public class UserControllerTests {
         assertTrue(repo.findById(newUser.getId()).isPresent());
     }
     @Test
-    public void testPutUser() {
+    public void testPutUser() throws Exception {
         User updatedUserData = new User();
-        updatedUserData.setNextPuzzle("Puzzle2");
-        updatedUserData.setCompleted(List.of("Puzzle0", "Puzzle1"));
+        updatedUserData.setNextPuzzle(2);
+        updatedUserData.setCompleted(List.of(0, 1));
 
-        ResponseEntity<User> response = controller.putUser(existingUser.getId(), updatedUserData);
+        User user = controller.putUser(existingUser.getId(), updatedUserData);
 
-        assertEquals(200, response.getStatusCodeValue());
-        User updatedUser = response.getBody();
-        assertNotNull(updatedUser);
-        assertEquals("Puzzle2", updatedUser.getNextPuzzle());
-        assertTrue(updatedUser.getCompleted().containsAll(List.of("Puzzle0", "Puzzle1")));
+        // Test that the HTTP Status Code is 200
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonBody = mapper.writeValueAsString(updatedUserData);
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Content-Type", "application/json")
+                .uri(URI.create("http://localhost:" + port + "/user/" + existingUser.getId()))
+                .PUT(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+
+        assertNotNull(user);
+        assertEquals(2, user.getNextPuzzle());
+        assertTrue(user.getCompleted().containsAll(List.of(0, 1)));
     }
     @Test
-    public void testPutUserNotFound() {
+    public void testPutUserNotFound() throws Exception {
         // Prepare updated user data
         User updatedUserData = new User();
-        updatedUserData.setNextPuzzle("Puzzle2");
-        updatedUserData.setCompleted(List.of("Puzzle0", "Puzzle1"));
+        updatedUserData.setNextPuzzle(2);
+        updatedUserData.setCompleted(List.of(0, 1));
 
         // Attempt to update a non-existing user
-        ResponseEntity<User> response = controller.putUser("nonExistingId", updatedUserData);
-
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonBody = mapper.writeValueAsString(updatedUserData);
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Content-Type", "application/json")
+                .uri(URI.create("http://localhost:" + port + "/user/" + "nonExistingId"))
+                .PUT(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         // Validate the response
-        assertEquals(404, response.getStatusCodeValue());
+        // Test for if the 404 error is thrown properly
+        assertEquals(404, response.statusCode());
     }
     @Test
-    public void testGetUser() {
-        ResponseEntity<User> response = controller.getUser(existingUser.getId());
-        assertEquals(200, response.getStatusCodeValue());
-        User foundUser = response.getBody();
-        assertNotNull(foundUser);
-        assertEquals(existingUser.getId(), foundUser.getId());
+    public void testGetUser() throws Exception {
+        // Perform GET
+        User user = controller.getUser(existingUser.getId());
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Content-Type", "application/json")
+                .uri(URI.create("http://localhost:" + port + "/user/" + existingUser.getId()))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        // Test status code 200
+        assertEquals(200, response.statusCode());
+
+        assertNotNull(user);
+        assertEquals(existingUser.getId(), user.getId());
     }
     @Test
-    public void testGetUserNotFound() {
-        ResponseEntity<User> response = controller.getUser("nonExistingId");
-        assertEquals(404, response.getStatusCodeValue());
+    public void testGetUserNotFound() throws Exception {
+        // Perform GET
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Content-Type", "application/json")
+                .uri(URI.create("http://localhost:" + port + "/user/" + "nonExistingId"))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(404, response.statusCode());
     }
     @Test
     public void testDeleteUser() {
